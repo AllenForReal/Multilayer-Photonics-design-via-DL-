dccca41c3c756ecb8aa9f2bec355525346a1bb38
allen zhang <allenzhang@cu-engr2-2-1-10.201.66.180.int.colorado.edu>
2022-05-18 09:58:55 -0600

    Prephase

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import React from 'react';
+class App extends React.Component
+{ render(){
+    return <h1>Hello</h1>
+  }
+
+}
+  // function App() {
+  // const name = ' Allen'
+  // return (
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <h1>
+  //       Hello From React: {name}
+  //     </h1>
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  // </div>
+  // );
+  // }
+
+export default App;
